e3b48133b0a991006c8d8f621fb76f33f5c5213f
gabdio95 <148788540+gabdio95@users.noreply.github.com>
2023-10-30 17:34:43 +0100

    Add files via upload

---
+++ esercitazioni-giorno1s2/js-folder/myfile.js
@@ -0,0 +1,18 @@
+/* risposta 1*/
+/*datatype principali sono: STRINGA(ovvero i suoi valori sono frasi, parole, lettere), valori che per essere renderizzati hanno bisogno usare i doppi o singoli apici(virgolette "");NUMERICO( i suoi valori sono numeri pieni o decimali, questi ultimi scritti usando il punto non la virgola), numeri singoli o in serie; BOOLEANO(valori di tipo binario,ovvero possono essere solo 2:true o false, si possono anche scrivere rispettivamente 1 o 0); NULL( serve a segnalare la voluta assenza di un ogetto, non equivale al valore numerico 0 poichè anche questo è un valore)*/
+/*risposta 2*/
+let myName="Gabriele"
+/*risposta 3*/
+let number1= 12
+let number2= 20
+console.log(number1+number2)
+/*risposta 4*/
+let x= 12
+
+/*risposta 5*/
+myName=-"diorio"
+
+/*risposta 6*/
+
+
+/*risposta 7*/
